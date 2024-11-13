eb238e026884439b2589613c7deaed2b2525f30b
salahifaoui <salahifaoui2022@gmail.com>
2024-11-13 11:02:41 +0100

    feat: implement message handling in TelegramAdapter to support typing indication and improve message processing

--- src/adapters/telegram.ts
+++ src/adapters/telegram.ts
@@ -33,7 +33,43 @@ export class TelegramAdapter extends BaseAdapter {
     this.client.command("start", async (ctx) => {
       await ctx.reply("Welcome!");
     });
-    
+    this.client.on("message", (ctx) => {
+      const sender: ISender = {
+        id: ctx.message.from.id.toString(),
+        name: ctx.message.from.username || ctx.message.from.first_name,
+        isDirect: true,
+      };
+      const message: IMessage = {
+        chatId: ctx.message.from.id.toString(), // TODO
+        text: ctx.message.text,
+        // taggedAccounts,
+        // roomName: roomMetadata.roomName,
+      };
+      console.log("========");
+      console.log("ctx", ctx);
+      console.log("========");
+      // const message = ctx.message;
+      console.log("message", message);
+      console.log("========");
+      ctx.replyWithChatAction('typing')
+      this.bot.onMessage(
+        sender,
+        message,
+        (reply) => {
+          console.log("========== BOT REPLY ==========");
+          console.log(reply);
+          ctx.reply(reply);
+          // ctx.reply(reply, { parse_mode: "HTML" });
+          this.client.api.sendMessage(ctx.message.chat.id, reply, { parse_mode: "HTML" });
+          // this.client.replyHtmlNotice(roomId, event, reply);
+          // this.client.setTyping(roomId, false);
+          return reply;
+        },
+        (isTyping) => {
+          // this.client.setTyping(roomId, isTyping);
+        },
+      );
+    });
     this.client.start();
 
     console.log("TelegramBot started");
